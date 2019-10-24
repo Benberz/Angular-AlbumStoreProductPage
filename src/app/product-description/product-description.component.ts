ea35b43add7fcb6520a8338e40282408280073d5
Softronix <softronix.android@gmail.com>
2019-10-24 11:57:37 +0100

    Completed module(2.1)

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from './../product.service';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
@@ -13,10 +13,8 @@ export class ProductDescriptionComponent implements OnInit {
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => {
-      console.log(response);
-      return this.albumInfo = response;
-    });
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response
+    );
   }
 
 }
